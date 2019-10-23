bf59e7339649f19ec6e8f2572a1d3ac3ffee61c7
Lushenggang <lushg-tcxg@qq.com>
2019-10-23 21:18:20 +0800

    publish

---
+++ push.sh
@@ -0,0 +1,15 @@
+git add .
+git commit -m publish
+git push
+ssh root@wintc.top << autoscript
+cd ~
+source env.sh
+cd /home/lushg/blog-backend
+source venv/bin/activate
+git pull origin master
+pip install -r requirement.txt
+lsof -i:5000 | grep 5000 | awk '{ print \$2 }' | xargs kill -9
+nohup python runserver --host 0.0.0.0 &
+exit
+autoscript
+echo 'done'
