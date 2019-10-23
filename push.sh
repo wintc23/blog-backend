34d1c5e810314b3c71a10fa086330cb872940868
Lushenggang <lushg-tcxg@qq.com>
2019-10-23 21:23:29 +0800

    publish

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ source venv/bin/activate
 git pull origin master
 pip install -r requirement.txt
 lsof -i:5000 | grep 5000 | awk '{ print \$2 }' | xargs kill -9
-nohup python main runserver --host 0.0.0.0 &
+nohup python main.py runserver --host 0.0.0.0 &
 exit
 autoscript
 echo 'done'
